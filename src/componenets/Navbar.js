26d3a76accbac36ef6f548d0171afce082481a31
Sakif Chowdhury <sakif.cho@gmail.com>
2022-05-31 17:36:00 -0400

    added navbar

---
+++ src/componenets/Navbar.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { MenuData } from '../data/MenuData'
+import { css } from 'styled-components'
+import { Button } from './Button'
+import { CgMenuRight } from 'react-icons/cg'
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0rem 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`
+
+const Logo = styled(Link)`
+  ${NavLink}
+`
+
+const MenuBars = styled(CgMenuRight)`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    color: white;
+    background-size: contain;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 25%);
+  }
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`
+
+const Navbtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <Logo to='/'>BROOKLYN</Logo>
+      <MenuBars />
+      <NavMenu>
+        {MenuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <Navbtn>
+        <Button to='/contact' primary='true'>
+          Contact Us
+        </Button>
+      </Navbtn>
+    </Nav>
+  )
+}
+
+export default Navbar
